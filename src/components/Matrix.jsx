2b7c01411a1bdc89b8899983d3ae0fa19b8f20a3
“codebydhae” <dhaesha3000@gmail.com>
2025-01-18 15:11:48 -0500

    project file structure implemented successfully

---
+++ src/components/Matrix.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import martix1 from "../assets/matrix.gif";
+
+function Matrix() {
+  return (
+    <>
+    <h3> test </h3>
+    <img src={martix1} alt="BigCo Inc. logo" height="" width=""/>
+    </>
+  );
+}
+
+export default Matrix;
